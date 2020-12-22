706a3c4fbf2f80946a59c03c6bb52e5131bf26e9
Mario Reid <mario.reid83@gmail.com>
2020-12-21 21:22:11 -0500

    working on 5day forecast

--- script.js
+++ script.js
@@ -61,15 +61,55 @@ $(document).ready(function () {
             console.log(queryURL);
             // $("city").text()
 
-            var currentDate=moment()
-            cityNameEL.text(response.name)
-            var tempF = ((response.main.temp-273.15)*1.8)+32
-            tempEL.text(tempF)
-            humidityEL.text(response.main.humidity)
-            windSpeedEL.text(response.wind.speed)
+          var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + APIKey;
+
+            $.ajax({
+              url: queryURL2,
+              method: "GET"
+            }).then(function(response2) {
+
+
+              console.log(response2);
+
+              var currentDate=moment().format('MM/DD/YYYY')
+              cityNameEL.text(response.name + ' (' + currentDate + ')')
+              var tempF = Math.round((((response.main.temp-273.15)*1.8)+32)*100)/100;
+              tempEL.text("Temperature: " + tempF + "°F")
+              humidityEL.text("Humidity: " + response.main.humidity)
+              windSpeedEL.text("Wind Speed: " + response.wind.speed)
+              uvIndexEL.text("UV index: " + response2.value)
+
+            })
 
           })
 
-         
+          //// the start of 5 day
+
+          var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q="+ newCity+"&appid=" + APIKey;
+
+          $.ajax({
+            url: queryURL3,
+            method: "GET"
+          }).then(function(response3) {
+            var day1Data = response3.list[0];
+
+            $('#day1-date').text(moment.unix(day1Data.dt).format('MM/DD/YYYY'));
+
+            $('#day1-temp').text(day1Data.main.temp);
+
+            $('#day1-humidity').text(day1Data.main.humidity);
+
+
+            var day2Data = response3.list[8];
+            var day3Data = response3.list[16];
+            var day4Data = response3.list[24];
+            var day5Data = response3.list[32];
+
+
+
+            console.log(day1Data);
+          })
+
+
         });
       });
